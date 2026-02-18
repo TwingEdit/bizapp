366f47d6c4ee74b344e7789de563f2a6419b6a15
TwingEdit <tuacha007@gmail.com>
2026-02-18 17:13:26 +0300

    Update sendPush.js

--- sendPush.js
+++ sendPush.js
@@ -1,30 +1,28 @@
-const webpush = require("web-push");
+const webpush = require('web-push');
 
 const vapidKeys = {
-  publicKey: "BB1e5kOUhOQjUW-i3_olooiI1Orafb7rKY-ETyeQ3A7smnDpr6OGAyq8o0opRT0iHqVPoLzptgHTtagmhfdMjCw",
-  privateKey: "UF6A-UDio7IpAuBrSiG3qJXpDToDUyvShQ5laR4TbAQ"
+  publicKey: 'BB1e5kOUhOQjUW-i3_olooiI1Orafb7rKY-ETyeQ3A7smnDpr6OGAyq8o0opRT0iHqVPoLzptgHTtagmhfdMjCw',
+  privateKey: 'UF6A-UDio7IpAuBrSiG3qJXpDToDUyvShQ5laR4TbAQ'
 };
 
 webpush.setVapidDetails(
-  "mailto:test@test.com",
+  'mailto:you@example.com',
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
 const subscription = {
-  "endpoint": "https://fcm.googleapis.com/fcm/send/e84UMc29kCE:APA91bEt3FVOlYKZcGdLpZ3cpLFeLcSOtjuarwwJaQtDdRwXlwdCUo6ocs-0sfPoLeaI-1GGdC1DKWBOXr6dLIsgXcSfJs89wWXxhIPqgvRtsxdf2uANUo9sg7_uhJ4-Iy1O9-1V5jdZ",
-  "expirationTime": null,
-  "keys": {
-    "p256dh": "BBgV_lT6bulBOL3eFuwlV6RcMX7iaqu3wL-z5s8twKhT4KDh7rl0ESrPL3M9DoLeohf9PNpE1WjOctOCSF7aFhA",
-    "auth": "5Hn4LlqkrTswQSfOr0xazA"
+  endpoint: "https://fcm.googleapis.com/fcm/send/e84UMc29kCE:APA91bEt3FVOlYKZcGdLpZ3cpLFeLcSOtjuarwwJaQtDdRwXlwdCUo6ocs-0sfPoLeaI-1GGdC1DKWBOXr6dLIsgXcSfJs89wWXxhIPqgvRtsxdf2uANUo9sg7_uhJ4-Iy1O9-1V5jdZ",
+  expirationTime: null,
+  keys: {
+    p256dh: "BBgV_lT6bulBOL3eFuwlV6RcMX7iaqu3wL-z5s8twKhT4KDh7rl0ESrPL3M9DoLeohf9PNpE1WjOctOCSF7aFhA",
+    auth: "5Hn4LlqkrTswQSfOr0xazA"
   }
 };
 
-const payload = JSON.stringify({
-  title: "💖 Seni Öptü!",
-  body: "Yeni bir öpücük geldi 💋"
-});
-
-webpush.sendNotification(subscription, payload)
-  .then(() => console.log("Bildirim gönderildi 🚀"))
-  .catch(err => console.error("Hata:", err));
+webpush.sendNotification(subscription, JSON.stringify({
+  title: 'Seni Öptü! 💋',
+  body: 'Yeni bir öpücük mesajı geldi 💖'
+}))
+.then(() => console.log('Bildirim gönderildi 🚀'))
+.catch(err => console.error('Push gönderilemedi ❌', err));
