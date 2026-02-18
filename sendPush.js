5378dd632f12b525ac4b51291b1815d7cdc3e9ca
TwingEdit <tuacha007@gmail.com>
2026-02-18 17:00:31 +0300

    Update sendPush.js

--- sendPush.js
+++ sendPush.js
@@ -1,27 +1,30 @@
-const webpush = require('web-push');
+const webpush = require("web-push");
 
 const vapidKeys = {
-    publicKey: 'BB1e5kOUhOQjUW-i3_olooiI1Orafb7rKY-ETyeQ3A7smnDpr6OGAyq8o0opRT0iHqVPoLzptgHTtagmhfdMjCw',
-    privateKey: 'UF6A-UDio7IpAuBrSiG3qJXpDToDUyvShQ5laR4TbAQ'
+  publicKey: "BB1e5kOUhOQjUW-i3_olooiI1Orafb7rKY-ETyeQ3A7smnDpr6OGAyq8o0opRT0iHqVPoLzptgHTtagmhfdMjCw",
+  privateKey: "UF6A-UDio7IpAuBrSiG3qJXpDToDUyvShQ5laR4TbAQ"
 };
 
 webpush.setVapidDetails(
-    'mailto:you@example.com',
-    vapidKeys.publicKey,
-    vapidKeys.privateKey
+  "mailto:test@test.com",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
 );
 
 const subscription = {
-    endpoint: "BURAYA_ENDPOINT_URL",
-    keys: {
-        p256dh: "BURAYA_P256DH_KEY",
-        auth: "BURAYA_AUTH_KEY"
-    }
+  "endpoint": "https://fcm.googleapis.com/fcm/send/e84UMc29kCE:APA91bEt3FVOlYKZcGdLpZ3cpLFeLcSOtjuarwwJaQtDdRwXlwdCUo6ocs-0sfPoLeaI-1GGdC1DKWBOXr6dLIsgXcSfJs89wWXxhIPqgvRtsxdf2uANUo9sg7_uhJ4-Iy1O9-1V5jdZ",
+  "expirationTime": null,
+  "keys": {
+    "p256dh": "BBgV_lT6bulBOL3eFuwlV6RcMX7iaqu3wL-z5s8twKhT4KDh7rl0ESrPL3M9DoLeohf9PNpE1WjOctOCSF7aFhA",
+    "auth": "5Hn4LlqkrTswQSfOr0xazA"
+  }
 };
 
-webpush.sendNotification(subscription, JSON.stringify({
-    title: 'Seni Öptü!',
-    body: '💖 Yeni bir öpücük mesajı geldi'
-}))
-.then(() => console.log('Push gönderildi ✅'))
-.catch(err => console.error('Push gönderilemedi ❌', err));
+const payload = JSON.stringify({
+  title: "💖 Seni Öptü!",
+  body: "Yeni bir öpücük geldi 💋"
+});
+
+webpush.sendNotification(subscription, payload)
+  .then(() => console.log("Bildirim gönderildi 🚀"))
+  .catch(err => console.error("Hata:", err));
